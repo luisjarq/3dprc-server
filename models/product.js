4fef8e0a2d82daad997a6d6236eee16ef1e13432
luisjarq <lc.luisja@gmail.com>
2022-02-15 20:38:06 +0100

    product update

--- models/product.js
+++ models/product.js
@@ -4,10 +4,13 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema(
   {
     name: { type: String, required: true },
+    brand: { type: String, required: true },
     description: { type: String, required: true },
     imgs: [{ type: mongoose.Types.ObjectId, ref: "Img" }],
     productParts: [{ type: mongoose.Types.ObjectId, ref: "ProductPart" }],
     price :{ type: Number, required: true },
+    discount :{ type: Number, required: true },    
+    stock :{ type: Number, required: true },    
     type :{ type: String, required: true },
 
   },
