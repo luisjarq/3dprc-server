abdfa02af35ec1ee0b43fb9d243ae8a300bb4e6e
luisjarq <lc.luisja@gmail.com>
2022-02-17 12:27:06 +0100

    3 legged auth

--- config/config.js
+++ config/config.js
@@ -5,5 +5,16 @@ const PORT = process.env.PORT || 5000;
 const DB_URL = process.env.DB_URL;
 const JWT_Secret = process.env.JWT_Secret;
 const JWT_AdminSecret = process.env.JWT_AdminSecret;
+const ClientId = process.env.forgeCId;
+const ClientSecret = process.env.forgeCSecret;
+const Callback = process.env.callback;
 
-module.exports = { DB_URL, PORT, JWT_Secret , JWT_AdminSecret};
+module.exports = {
+  DB_URL,
+  PORT,
+  JWT_Secret,
+  JWT_AdminSecret,
+  ClientId,
+  ClientSecret,
+  Callback,
+};
--- routers/callbackForge.router.js
+++ routers/callbackForge.router.js
@@ -1,17 +1,59 @@
 const express = require("express");
 const router = express.Router();
+const { config } = require("dotenv");
+var ForgeSDK = require("forge-apis");
+const { ClientId, ClientSecret, Callback } = require("../config/config");
+let oAuth2ThreeLegged;
 
-function callbackToken(req,res,next){
-    //console.log(req);
-    return res.status(201).json(req.query.code);
+function receiveToken(req, res, next) {
+  //console.log(req);
+  oAuth2ThreeLegged.getToken(req.query.code).then(
+    function (credentials) {
+      res.status(201).json(credentials);
+    },
+    function (err) {
+      console.error(err);
+    }
+  );
 }
-function receiveToken(req,res,next){
-    //console.log(req);
-    return res.status(201).json(req.body);
+function authorize(req, res, next) {
+  // Initialize the 3-legged OAuth2 client, set specific scopes and optionally set the `autoRefresh` parameter to true
+  // if you want the token to auto refresh
+  let autoRefresh = true;
+  oAuth2ThreeLegged = new ForgeSDK.AuthClientThreeLegged(
+    ClientId,
+    ClientSecret,
+    Callback,
+    ["data:read"],
+    autoRefresh
+  );
+
+  // Generate a URL page that asks for permissions for the specified scopes.
+  res.redirect(oAuth2ThreeLegged.generateAuthUrl());
+}
+function authorize2L(req, res, next) {
+  let autoRefresh = true; // or false
+
+  let oAuth2TwoLegged = new ForgeSDK.AuthClientTwoLegged(
+    ClientId,
+    ClientSecret,
+    ["data:read"],
+    autoRefresh
+  );
+
+  oAuth2TwoLegged.authenticate().then(
+    function (credentials) {
+      res.status(201).json(credentials);
+    },
+    function (err) {
+      console.error(err);
+    }
+  );
 }
 //const { getByUser } = require("../controllers/cart.controller");
 // READ OPERATIONS
-router.get("/",callbackToken);
+router.get("/authorize3L", authorize);
+router.get("/authorize2L", authorize2L);
 // CREATE OPERATION
 router.post("/", receiveToken);
 // UPDATE OPERATION
